234dd79ba93c2ee5bfa8c337c0497c1f639f8d1e
tombuildsstuff <git@ibuildstuff.co.uk>
2020-05-04 09:19:00 +0200

    adding stdout

--- main_test.go
+++ main_test.go
@@ -1,17 +1,23 @@
 package main
 
-import "testing"
+import (
+	"log"
+	"testing"
+)
 
 func TestPassing(t *testing.T) {
+	log.Printf("stdout - from the passing test")
 	t.Log("This should always pass")
 }
 
 func TestSkipped(t *testing.T) {
+	log.Printf("stdout - from the skipped test")
 	t.Log("This should be skipped")
 	t.Skip("Skipping because we should skip it")
 }
 
 func TestFailed(t *testing.T) {
+	log.Printf("stdout - from the failed test")
 	t.Log("This should always fail")
 	t.Fail()
 }
