312fb5370003c3b2cf50d47e4084cfe6e2429235
tombuildsstuff <git@ibuildstuff.co.uk>
2020-05-04 11:49:58 +0200

    adding a terraform test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tombuildsstuff/tc-test-parser-bug
+
+go 1.14
+
+require github.com/hashicorp/terraform-plugin-sdk v1.6.0
--- main_test.go
+++ main_test.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestPassing(t *testing.T) {
@@ -24,4 +29,55 @@ func TestFailed(t *testing.T) {
 	log.Printf("stdout - from the failed test")
 	t.Log("This should always fail")
 	t.Fail()
+}
+
+func TestTerraformRunner(t *testing.T) {
+	var print = func(words string, instance int) {
+		os.Stderr.WriteString(fmt.Sprintf("stderr - %d - %s\n", instance, words))
+		log.Printf("stdout - %d, %s", instance, words)
+	}
+
+	for i := 0; i < 5; i++ {
+		t.Run(fmt.Sprintf("Instance %d", i+1), func(t *testing.T) {
+			resource.Test(t, resource.TestCase{
+				PreCheck:     func() {
+					print("precheck", i)
+				},
+				Providers:    map[string]terraform.ResourceProvider{
+					"rick": &schema.Provider{
+						DataSourcesMap: map[string]*schema.Resource{
+							"rick_and_morty": {
+								Read: func(data *schema.ResourceData, i interface{}) error {
+									data.Set("test", "bar")
+									return nil
+								},
+								Schema: map[string]*schema.Schema{
+									"test": {
+										Type:  schema.TypeString,
+										Computed:  true,
+									},
+								},
+							},
+						},
+						ResourcesMap: make(map[string]*schema.Resource),
+					},
+				},
+				CheckDestroy: func(state *terraform.State) error {
+					print("destroy", i)
+					return nil
+				},
+				Steps: []resource.TestStep{
+					{
+						Config: `data "rick_and_morty" "test" { }`,
+						Check: resource.ComposeTestCheckFunc(
+							func(state *terraform.State) error {
+								print("check func", i)
+								return nil
+							},
+						),
+					},
+				},
+			})
+		})
+	}
 }
