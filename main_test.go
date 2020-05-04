b65da7f2aab8fb935f65f466a8fb299281442437
tombuildsstuff <git@ibuildstuff.co.uk>
2020-05-04 11:14:33 +0200

    adding stderr

--- main_test.go
+++ main_test.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"log"
+	"os"
 	"testing"
 )
 
 func TestPassing(t *testing.T) {
+	os.Stderr.WriteString("stderr - from the passing test\n")
 	log.Printf("stdout - from the passing test")
 	t.Log("This should always pass")
 }
 
 func TestSkipped(t *testing.T) {
+	os.Stderr.WriteString("stderr - from the skipped test\n")
 	log.Printf("stdout - from the skipped test")
 	t.Log("This should be skipped")
 	t.Skip("Skipping because we should skip it")
 }
 
 func TestFailed(t *testing.T) {
+	os.Stderr.WriteString("stderr - from the failed test\n")
 	log.Printf("stdout - from the failed test")
 	t.Log("This should always fail")
 	t.Fail()
